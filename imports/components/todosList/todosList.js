7f1a6d5d67301183ca802417dc291f93f7145ccc
Jie Tian <jt66@njit.edu>
2016-07-05 23:05:41 -0400

    Adding secure methods

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -45,26 +45,17 @@ class TodosListCtrl {
 
     addTask(newTask) {
         // Insert a task into the collection
-        Tasks.insert({
-            text: newTask,
-            createdAt: new Date,
-            owner: Meteor.userId(),
-            username: Meteor.user().username
-        });
+        Meteor.call('tasks.insert', newTask);
 
         // Clear form
         this.newTask = '';
     }
     removeTask(task) {
-        Tasks.remove(task._id);
+        Meteor.call('tasks.remove', task._id);
     }
 
     setChecked(task) {
-        Tasks.update(task._id, {
-            $set: {
-                checked: !task.checked
-            },
-        });
+        Meteor.call('tasks.setChecked', task._id, !task.checked);
     }
 }
 
